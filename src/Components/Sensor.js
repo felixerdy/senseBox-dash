4eb880e97c67bb654bdc63d3d686e0c55497954c
felixerdy <felix.erdmann@uni-muenster.de>
2019-01-14 23:04:14 +0100

    toggle panels

--- src/Components/Sensor.js
+++ src/Components/Sensor.js
@@ -1,60 +1,100 @@
-import React, { Component } from 'react';
-import { Sparklines, SparklinesCurve } from 'react-sparklines';
-import './Sensor.css'
+import React, { Component } from "react";
+import { Sparklines, SparklinesCurve } from "react-sparklines";
+import "./Sensor.scss";
 
 const colors = {
-    "Temperatur": "gold",
-    "rel. Luftfeuchte": "mediumturquoise",
-    "Beleuchtungsstärke": "yellow",
-    "UV-Intensität": "palegreen",
-    "Luftdruck": "lightblue",
-    "Lautstärke": "thistle",
-    "PM2.5": "gainsboro",
-    "PM10": "lightgray"
-}
+  Temperatur: "gold",
+  "rel. Luftfeuchte": "mediumturquoise",
+  Beleuchtungsstärke: "yellow",
+  "UV-Intensität": "palegreen",
+  Luftdruck: "lightblue",
+  Lautstärke: "thistle",
+  "PM2.5": "gainsboro",
+  PM10: "lightgray"
+};
 
 class Sensor extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-          data: []
-        }
+  constructor(props) {
+    super(props);
 
-        this.alertMinMax = this.alertMinMax.bind(this)
-      }
+    this.state = {
+      data: [],
+      detailActive: false
+    };
+
+    this.alertMinMax = this.alertMinMax.bind(this);
+  }
 
   componentDidMount() {
-    fetch(`https://api.opensensemap.org/boxes/${this.props.boxId}/data/${this.props._id}`)
-    .then(res => res.json())
-    .then(data => this.setState({data: data}))
+    fetch(
+      `https://api.opensensemap.org/boxes/${this.props.boxId}/data/${
+        this.props._id
+      }`
+    )
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ data: data });
+      });
   }
 
   componentDidUpdate(prevProps) {
-    if(this.props.lastMeasurement.value !== prevProps.lastMeasurement.value) {
-      fetch(`https://api.opensensemap.org/boxes/${this.props.boxId}/data/${this.props._id}`)
-      .then(res => res.json())
-      .then(data => this.setState({data: data}))
+    if (this.props.lastMeasurement.value !== prevProps.lastMeasurement.value) {
+      fetch(
+        `https://api.opensensemap.org/boxes/${this.props.boxId}/data/${
+          this.props._id
+        }`
+      )
+        .then(res => res.json())
+        .then(data => this.setState({ data: data }));
     }
   }
 
   alertMinMax() {
-    let min = Math.min(...this.state.data.map(d => Number(d.value))).toFixed(2)
-    let max = Math.max(...this.state.data.map(d => Number(d.value))).toFixed(2)
+    let min = Math.min(...this.state.data.map(d => Number(d.value))).toFixed(2);
+    let max = Math.max(...this.state.data.map(d => Number(d.value))).toFixed(2);
 
-    let alertText = `48 Stunden Minimum: ${min} ${this.props.unit}\n48 Stunden Maximum: ${max} ${this.props.unit}`
+    let alertText = `48 Stunden Minimum: ${min} ${
+      this.props.unit
+    }\n48 Stunden Maximum: ${max} ${this.props.unit}`;
 
-    alert(alertText)
+    alert(alertText);
   }
 
   render() {
     return (
-      <div className="sensor" onClick={this.alertMinMax}>
-        <Sparklines data={this.state.data.map(d => d.value).filter((e, i) => i % 40 === 0).reverse()} className="sparkLine" margin={0}>
+      <div className="sensor">
+        <div
+          className="info"
+          onClick={() =>
+            this.setState({ detailActive: !this.state.detailActive })
+          }
+        >
+          <Sparklines
+            data={this.state.data
+              .map(d => d.value)
+              .filter((e, i) => i % 40 === 0)
+              .reverse()}
+            className="sparkLine"
+            margin={0}
+          >
             <SparklinesCurve color={colors[this.props.title] || "lightblue"} />
-        </Sparklines>
-        <p className="phenomenon">{this.props.title}</p>
-        <p className="measurement">{parseFloat(this.props.lastMeasurement.value).toFixed(2)} {this.props.unit}</p>
+          </Sparklines>
+          <p className="phenomenon">{this.props.title}</p>
+          <p className="measurement">
+            {parseFloat(this.props.lastMeasurement.value).toFixed(2)}{" "}
+            {this.props.unit}
+          </p>
+        </div>
+        <div className={`details ${this.state.detailActive ? "active" : ""}`}>
+          {`Min: ${Math.min(
+            ...this.state.data.map(d => Number(d.value))
+          ).toFixed(2)} ${this.props.unit}`}
+          <br />
+          {`Max: ${Math.max(
+            ...this.state.data.map(d => Number(d.value))
+          ).toFixed(2)} ${this.props.unit}`}
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,22 +5,24 @@ import App from "./Components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// Activate the service worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    let swPath = `service-worker.js`;
-    navigator.serviceWorker.register(swPath).then(
-      function(registration) {
-        // Registration was successful
-        console.log(
-          "ServiceWorker registration successful with scope: ",
-          registration.scope
-        );
-      },
-      function(err) {
-        // registration failed :(
-        console.log("ServiceWorker registration failed: ", err);
-      }
-    );
-  });
+if (process.env.NODE_ENV != "development") {
+  // Activate the service worker
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      let swPath = `service-worker.js`;
+      navigator.serviceWorker.register(swPath).then(
+        function(registration) {
+          // Registration was successful
+          console.log(
+            "ServiceWorker registration successful with scope: ",
+            registration.scope
+          );
+        },
+        function(err) {
+          // registration failed :(
+          console.log("ServiceWorker registration failed: ", err);
+        }
+      );
+    });
+  }
 }
